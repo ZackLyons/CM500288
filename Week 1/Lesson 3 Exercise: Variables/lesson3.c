8df73218aa4348f3a31e8a7523eb1361d057ab06
ZackLyons <zl221@bath.ac.uk>
2024-09-05 14:01:47 +0000

    Pending changes exported from your codespace

---
+++ Week 1/Lesson 3 Exercise: Variables/lesson3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* 
+ * In the code below,
+ * 
+ * 1. change the value stored in n to 24
+ *
+ * Now, using the printf function in the stdio.h library (imported above),
+ * 
+ * 2. print out the value of n 
+ * 3. change the value of n to 4 and in a new variable, 
+ * store the value of n multiplied by 3 and print it out
+ *
+*/
+
+int main(void) {
+  int n = 14;
+  printf("Hello World\n");
+  return 0;
+}
